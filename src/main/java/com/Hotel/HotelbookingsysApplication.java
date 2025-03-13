9cdb73ac56b4d44b2571a39d0cd8a710b83797c2
Prabu <prabu1372005@gmail.com>
2025-03-13 22:05:54 +0530

    add project

---
+++ src/main/java/com/Hotel/HotelbookingsysApplication.java
@@ -0,0 +1,13 @@
+package com.Hotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelbookingsysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelbookingsysApplication.class, args);
+	}
+
+}
