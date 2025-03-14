794efe1e2ea97e641ed68385e03de9097a902b95
Prabu <prabu1372005@gmail.com>
2025-03-14 09:21:32 +0530

    enttiy done

--- src/main/java/com/Hotel/Entity/CustomerEntity.java
+++ src/main/java/com/Hotel/Entity/CustomerEntity.java
@@ -1,5 +1,40 @@
 package com.Hotel.Entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "CustomerDetails")
 public class CustomerEntity {
 
+	@Id
+    private Long userId;  
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email", unique = true)
+    private String email;
+
+    @Column(name = "mobile_number")
+    private String mobileNumber;  
+
+    @Column(name = "age")
+    private Integer age;  
+
+    @Column(name = "gender")
+    private String gender;
 }
